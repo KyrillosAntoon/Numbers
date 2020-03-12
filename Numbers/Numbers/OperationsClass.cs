d5a5163bf47977f1d32433fb714da443cfb734a0
HunterSteele <hunter.steele.1@gmail.com>
2020-03-11 19:49:55 -0500

    -Init

---
+++ Numbers/Numbers/OperationsClass.cs
@@ -0,0 +1,89 @@
+﻿namespace Opeartions
+{
+    static class OperationsClass
+    {
+        /// <summary>
+        /// Function that returns the sum of 3 numbers
+        /// </summary>
+        public static double Sum(double a, double b, double c)
+        {
+            return a + b + c;
+        }
+
+        /// <summary>
+        /// Function that returns the Product of 3 numbers
+        /// </summary>
+        public static double Product(double a, double b, double c)
+        {
+            return a * b * c;
+        }
+
+        /// <summary>
+        /// Function that returns the sum of 3 numbers
+        /// </summary>
+        public static double Average(double a, double b, double c)
+        {
+            return (a + b + c) / 3;
+        }
+        /// <summary>
+        /// Compares the 3 numbers that are given, and modifies...
+        /// ...the greatest and smallest variables to contain a correct answer
+        /// </summary>
+        public static void Compare(double a, double b, double c, ref double greatest, ref double smallest)
+        {
+            if (a != b)                             
+            {
+                if (a > b)
+                {
+                    if (c > a)
+                    {
+                        greatest = c;
+                        smallest = b;
+                    }
+                    else
+                    {
+                        greatest = a;
+                        if (b > c)
+                        {
+                            smallest = c;
+                        }
+                        else
+                        {
+                            smallest = b;
+                        }
+                    }
+                }
+                else
+                {
+                    if (c > b)
+                    {
+                        greatest = c;
+                        smallest = a;
+                    }
+                    else
+                    {
+                        greatest = b;
+                        if (a > c)
+                        {
+                            smallest = c;
+                        }
+                        else
+                        {
+                            smallest = a;
+                        }
+                    }
+                }
+            }
+            else if (c > a)
+            {
+                greatest = c;
+                smallest = a;
+            }
+            else
+            {
+                greatest = a;
+                smallest = c;
+            }
+        }
+    }
+}
---
+++ Numbers/Numbers/Program.cs
@@ -0,0 +1,73 @@
+﻿// Kyrillos "Chris" Antoon
+// Numbers Problem from C# 2561
+// 01/31/2019
+
+using Opeartions;
+using System;
+
+namespace NumbersProblem
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            double first = 0, second = 0, third = 0, greatest = 0, smallest = 0;     // Variables to store user input & results
+            bool quit = false;                                                       // Checks if user quit
+
+            do
+            {
+
+                try
+                {
+                    Console.WriteLine("Please enter three " +
+                        "numbers below (0 for all to quit).");                      // Instruct user then take input
+
+                    Console.Write("Please enter the first number: ");               // Prompt and read in value 1
+                    first = System.Convert.ToDouble(Console.ReadLine());
+
+                    Console.Write("Please enter the second number: ");              // Prompt and read in value 2
+                    second = System.Convert.ToDouble(Console.ReadLine());
+
+                    Console.Write("Please enter the third number: ");               // Prompt and read in value 3
+                    third = System.Convert.ToDouble(Console.ReadLine());
+
+                    if (first != 0 && second != 0 && third != 0)                    // Check if user quit
+                    {
+                        Console.WriteLine("\n\n\n");                                // Formatting
+
+                        Console.WriteLine("The sum of the numbers are: "            // Print out the sum
+                            + OperationsClass.Sum(first, second, third).ToString()
+                            + " \n\n");
+
+                        Console.WriteLine("The product of the numbers are: " +      // Print out the product
+                            OperationsClass.Product(first, second, third).ToString()
+                            + " \n\n");
+
+                        Console.WriteLine("The Average of the numbers are: " +      // Print out the average
+                            OperationsClass.Average(first, second, third).ToString()
+                            + " \n\n");
+
+                        OperationsClass.Compare(first, second, third, ref greatest, ref smallest); // Call the Compare method to determine the greatest and smallest
+
+                        Console.WriteLine("The greatest number is: "                // Print out the greatest number
+                            + greatest + "\n ");
+
+                        Console.WriteLine("The smallest number is: "                // Print out the smallest number
+                            + smallest + "\n ");
+                    }
+                    else
+                    {
+                        quit = true;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Invalid input");
+                }
+            } while (!quit);                  // Check if user did not quit
+
+            Console.WriteLine("Press any key to continue");
+            Console.ReadLine();
+        }
+    }
+}
